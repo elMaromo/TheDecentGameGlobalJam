d279bbe6c66d0d2cf945ccf8ae23af75e43bf5d1
Javierdds <deduenassantanojavier@gmail.com>
2024-01-27 13:25:25 +0100

    feat: NPCs look at player

---
+++ Assets/Scripts/NPC/Npc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace NPC
+{
+    public class Npc : MonoBehaviour
+    {
+        private Transform _myTransform;
+        private Transform _player;
+
+        private void Awake()
+        {
+            _myTransform = transform;
+        }
+
+        public void Configure(Transform player)
+        {
+            _player = player;
+        }
+
+        private void Update()
+        {
+            LookAtPlayer();
+        }
+
+        private void LookAtPlayer()
+        {
+            var directionToPlayer = _player.position - _myTransform.position;
+            directionToPlayer.y = 0;
+            var rotation = Quaternion.LookRotation(directionToPlayer);
+            _myTransform.rotation = rotation * Quaternion.Euler(90, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/NPC/NpcsInstaller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace NPC
+{
+    public class NpcsInstaller : MonoBehaviour
+    {
+        [SerializeField] private Transform _player;
+        [SerializeField] private Npc[] _people;
+
+        private void Awake()
+        {
+            foreach (var person in _people)
+            {
+                person.Configure(_player);
+            }
+        }
+    }
+}
