618ca26c38507c0b3c4c6336dfad1b4770fc6f67
Javierdds <deduenassantanojavier@gmail.com>
2024-01-28 14:43:07 +0100

    feat: main menu loads lobby scene when play button is clicked

---
+++ Assets/Scripts/Utils/SceneLoaderButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Utils
+{
+    public class SceneLoaderButton : MonoBehaviour
+    {
+        [SerializeField] private string _sceneNameToLoad;
+        [SerializeField] private Button _button;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(LoadScene);
+        }
+
+        private void LoadScene()
+        {
+            SceneManager.LoadSceneAsync(_sceneNameToLoad);
+        }
+    }
+}
